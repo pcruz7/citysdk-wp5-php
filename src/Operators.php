d971f2ebcc916ccf03d568b9dc6ee45b64f75d51
pcruz7 <pedro.cruz.sousa@gmail.com>
2013-05-04 22:30:21 +0100

    added copyright

--- src/Operators.php
+++ src/Operators.php
@@ -2,20 +2,20 @@
 /*
  * COPYRIGHT NOTICE:
  *
- * This file is part of CitySDK WP5 Tourism Java Library.
+ * This file is part of CitySDK WP5 Tourism PHP Library.
  *
- * CitySDK WP5 Tourism Java Library is free software: you can redistribute it and/or modify
+ * CitySDK WP5 Tourism PHP Library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * CitySDK WP5 Tourism Java Library is distributed in the hope that it will be useful,
+ * CitySDK WP5 Tourism PHP Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with CitySDK WP5 Tourism Java Library. If not, see <http://www.gnu.org/licenses/>.
+ * along with CitySDK WP5 Tourism PHP Library. If not, see <http://www.gnu.org/licenses/>.
  *
  * Copyright 2013, IST
  */
